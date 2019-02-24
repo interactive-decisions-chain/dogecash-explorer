31dcef4038e110ae3d34e33c108e67aa64104e3e
Akshay <akshaycm@hotmail.com>
2019-02-24 19:45:23 +0300

    changes

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -5,9 +5,9 @@ import Card from './Card';
 
 const CardExchanges = () => (
   <Card title="Exchanges">
-    <a href="https://www.cryptopia.co.nz/Exchange/?market=BWK_BTC" target="_blank">Cryptopia</a><br />
+    <a href="https://www.cryptopia.co.nz/Exchange/?market=DOGEC_BTC" target="_blank">Cryptopia</a><br />
     <a href="https://crypto-bridge.org/" target="_blank">CryptoBridge</a><br />
-    <a href="https://txbit.io/Trade/BWK/BTC" target="_blank">Txbit.io</a><br />
+    <a href="https://txbit.io/Trade/DOGEC/BTC" target="_blank">Txbit.io</a><br />
     <a href="https://blocknet.co/" target="_blank">Blocknet</a><br />
   </Card>
 );
