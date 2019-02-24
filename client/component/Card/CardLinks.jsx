f2f787e6bcd88962e7383e37f9f98e6abe80825c
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-02-24 09:22:34 -0600

    some link updates needs more

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -5,13 +5,13 @@ import Card from './Card';
 
 const CardLinks = () => (
   <Card title="Links">
-    <a href="https://bulwarkcrypto.com/" target="_blank">Website</a><br />
+    <a href="https://dogec.io/" target="_blank">Website</a><br />
     <a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank">Bitcointalk</a><br />
-    <a href="https://github.com/bulwark-crypto" target="_blank">Github</a><br />
-    <a href="https://www.reddit.com/r/BulwarkCoin/" target="_blank">Reddit</a><br />
+    <a href="https://github.com/dogecash/dogecash" target="_blank">Github</a><br />
+    <a href="https://www.reddit.com/r/DogeCash/" target="_blank">Reddit</a><br />
     <a href="https://discord.gg/a7vhegP" target="_blank">Discord</a><br />
     <a href="https://t.me/bulwarkcrypto" target="_blank">Telegram</a><br />
-    <a href="https://twitter.com/BulwarkCoin" target="_blank">Twitter</a><br />
+    <a href="https://twitter.com/DogeCashCoin" target="_blank">Twitter</a><br />
     <a href="https://www.facebook.com/bulwark.coin.IO/" target="_blank">Facebook</a><br />
     <a href="https://medium.com/@bulwarktube" target="_blank">Medium</a>
   </Card>
