9443e5cc49bd1262e2704593ee0f4b2a4309f015
Akshay <akshaycm@hotmail.com>
2019-02-24 23:48:26 +0300

    changes to links

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -6,14 +6,13 @@ import Card from './Card';
 const CardLinks = () => (
   <Card title="Links">
     <a href="https://dogec.io/" target="_blank">Website</a><br />
-    <a href="https://bitcointalk.org/" target="_blank">Bitcointalk</a><br />
+    {/* <a href="https://bitcointalk.org/" target="_blank">Bitcointalk</a><br /> */}
     <a href="https://github.com/dogecash/dogecash" target="_blank">Github</a><br />
     <a href="https://www.reddit.com/r/DogeCash/" target="_blank">Reddit</a><br />
-    <a href="https://discord.gg/X2fAMg" target="_blank">Discord</a><br />
-    <a href="https://t.me/dogecashcoin" target="_blank">Telegram</a><br />
-    <a href="https://twitter.com/DogeCashCoin" target="_blank">Twitter</a><br />
-    <a href="https://www.facebook.com/bulwark.coin.IO/" target="_blank">Facebook</a><br />
-    <a href="https://steemit.com/@dogecash" target="_blank">Steemit</a>
+    <a href="https://discord.dogec.io/" target="_blank">Discord</a><br />
+    <a href="https://telegram.dogec.io/" target="_blank">Telegram</a><br />
+    <a href="https://twitter.dogec.io/" target="_blank">Twitter</a><br />
+    <a href="https://steemit.dogec.io/" target="_blank">Steemit</a>
   </Card>
 );
 
