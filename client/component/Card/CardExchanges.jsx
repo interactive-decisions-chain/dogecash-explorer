62b389a96b25e6d8c1097d59b9f8332bfe22b8b9
Akshay <akshaycm@hotmail.com>
2019-02-24 23:45:16 +0300

    changes

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -5,10 +5,10 @@ import Card from './Card';
 
 const CardExchanges = () => (
   <Card title="Exchanges">
-    <a href="https://www.cryptopia.co.nz/Exchange/?market=DOGEC_BTC" target="_blank">Cryptopia</a><br />
-    <a href="https://crypto-bridge.org/" target="_blank">CryptoBridge</a><br />
-    <a href="https://txbit.io/Trade/DOGEC/BTC" target="_blank">Txbit.io</a><br />
-    <a href="https://blocknet.co/" target="_blank">Blocknet</a><br />
+
+    <a href="https://cb.dogec.io/" target="_blank">CryptoBridge</a><br />
+    <a href="https://stex.dogec.io/" target="_blank">STEX</a><br />
+
   </Card>
 );
 
--- client/component/Footer.jsx
+++ client/component/Footer.jsx
@@ -54,27 +54,25 @@ class Footer extends Component {
           <div className="footer__social-media-wrapper">
             <div className="footer__social-title">Social Media</div>
             <div>
-              <a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank">
+              {/* <a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank">
                 <Icon name="bitcoin" className="fab footer__social-media-icon" />
-              </a>
-              <a href="https://discord.gg/a7vhegP" target="_blank">
+              </a> */}
+              <a href="https://discord.dogec.io/" target="_blank">
                 <Icon name="discord" className="fab footer__social-media-icon" />
               </a>
-              <a href="https://t.me/bulwarkcrypto" target="_blank">
+              <a href="https://telegram.dogec.io/" target="_blank">
                 <Icon name="telegram" className="fab footer__social-media-icon" />
               </a>
-              <a href="https://www.reddit.com/r/DogeCashCoin/" target="_blank">
+              <a href="https://reddit.dogec.io/" target="_blank">
                 <Icon name="reddit" className="fab footer__social-media-icon" />
               </a>
-              <a href="https://github.com/bulwark-crypto" target="_blank">
+              <a href="https://github.dogec.io/" target="_blank">
                 <Icon name="github" className="fab footer__social-media-icon" />
               </a>
-              <a href="https://twitter.com/DogeCashCoin" target="_blank">
+              <a href="https://twitter.dogec.io/" target="_blank">
                 <Icon name="twitter" className="fab footer__social-media-icon" />
               </a>
-              <a href="http://facebook.com/bulwark.coin.IO/" target="_blank">
-                <Icon name="facebook" className="fab footer__social-media-icon" />
-              </a>
+            
             </div>
           </div>
         </div>
