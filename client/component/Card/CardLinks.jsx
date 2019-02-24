e436b5d7d51620127a33eb056b91676fd5858260
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-02-24 09:32:06 -0600

    Update CardLinks.jsx

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -6,14 +6,14 @@ import Card from './Card';
 const CardLinks = () => (
   <Card title="Links">
     <a href="https://dogec.io/" target="_blank">Website</a><br />
-    <a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank">Bitcointalk</a><br />
+    <a href="https://bitcointalk.org/" target="_blank">Bitcointalk</a><br />
     <a href="https://github.com/dogecash/dogecash" target="_blank">Github</a><br />
     <a href="https://www.reddit.com/r/DogeCash/" target="_blank">Reddit</a><br />
-    <a href="https://discord.gg/a7vhegP" target="_blank">Discord</a><br />
-    <a href="https://t.me/bulwarkcrypto" target="_blank">Telegram</a><br />
+    <a href="https://discord.gg/X2fAMg" target="_blank">Discord</a><br />
+    <a href="https://t.me/dogecashcoin" target="_blank">Telegram</a><br />
     <a href="https://twitter.com/DogeCashCoin" target="_blank">Twitter</a><br />
     <a href="https://www.facebook.com/bulwark.coin.IO/" target="_blank">Facebook</a><br />
-    <a href="https://medium.com/@bulwarktube" target="_blank">Medium</a>
+    <a href="https://steemit.com/@dogecash" target="_blank">Steemit</a>
   </Card>
 );
 
