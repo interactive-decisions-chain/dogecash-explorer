1167e1036b1f9b0f4d8d5056d988055b435a9807
Akshay <akshaycm@hotmail.com>
2019-03-28 02:07:27 +0300

    [API] add GET coin/month data

--- server/route/api.js
+++ server/route/api.js
@@ -12,6 +12,7 @@ router.get('/block/:hash', blockex.getBlock);
 router.get('/coin', blockex.getCoin);
 router.get('/coin/history', blockex.getCoinHistory);
 router.get('/coin/week', blockex.getCoinsWeek());
+router.get('/coin/month', blockex.getCoinsMonth());
 router.get('/masternode', blockex.getMasternodes);
 router.get('/masternode/average', blockex.getAvgMNTime());
 router.get('/masternode/:hash', blockex.getMasternodeByAddress);
