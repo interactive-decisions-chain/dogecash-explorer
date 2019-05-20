307280069aedd9ae83febe7591b6279c973b0209
Akshay <akshaycm@hotmail.com>
2019-05-20 21:31:41 +0300

    [Backend] Fix MN count in proposal CRON

--- cron/proposal.js
+++ cron/proposal.js
@@ -10,60 +10,60 @@ const moment = require('moment');
 const Proposal = require('../model/proposal');
 
 async function syncProposal() {
-  const date = moment().utc().startOf('minute').toDate();
+    const date = moment().utc().startOf('minute').toDate();
 
-  await Proposal.remove({});
+    await Proposal.remove({});
 
-  rpc.timeout(10000); // 10 secs
+    rpc.timeout(10000); // 10 secs
 
-  const pps = await rpc.call('getbudgetinfo');
-  const mnc = await rpc.call('getmasternodecount');
-  const inserts = [];
-  await forEach(pps, async (pp) => {
-    const proposal = new Proposal({
-	  name: pp.Name,	
-    created: date,
-	  yeas: pp.Yeas,
-	  nays: pp.Nays,
-    status: ((pp.Yeas-pp.Nays)*10>mnc),
-	  budgetTotal: pp.TotalPayment,
-	  budgetMonthly: pp.MonthlyPayment,
-	  budgetPeriod: pp.RemainingPaymentCount,
-	  hash: pp.Hash,
-	  feehash: pp.FeeHash,
-      url: pp.URL,
+    const pps = await rpc.call('getbudgetinfo');
+    const mnc = await rpc.call('getmasternodecount');
+    const inserts = [];
+    await forEach(pps, async(pp) => {
+        const proposal = new Proposal({
+            name: pp.Name,
+            created: date,
+            yeas: pp.Yeas,
+            nays: pp.Nays,
+            status: ((pp.Yeas - pp.Nays) * 10 > mnc.total),
+            budgetTotal: pp.TotalPayment,
+            budgetMonthly: pp.MonthlyPayment,
+            budgetPeriod: pp.RemainingPaymentCount,
+            hash: pp.Hash,
+            feehash: pp.FeeHash,
+            url: pp.URL,
+        });
+
+        inserts.push(proposal);
     });
 
-    inserts.push(proposal);
-  });
-
-  if (inserts.length) {
-    await Proposal.insertMany(inserts);
-  }
+    if (inserts.length) {
+        await Proposal.insertMany(inserts);
+    }
 }
 
 /**
  * Handle locking.
  */
 async function update() {
-  const type = 'proposal';
-  let code = 0;
+    const type = 'proposal';
+    let code = 0;
 
-  try {
-    locker.lock(type);
-    await syncProposal();
-  } catch(err) {
-    console.log(err);
-    code = 1;
-  } finally {
     try {
-      locker.unlock(type);
-    } catch(err) {
-      console.log(err);
-      code = 1;
+        locker.lock(type);
+        await syncProposal();
+    } catch (err) {
+        console.log(err);
+        code = 1;
+    } finally {
+        try {
+            locker.unlock(type);
+        } catch (err) {
+            console.log(err);
+            code = 1;
+        }
+        exit(code);
     }
-    exit(code);
-  }
 }
 
 update();
