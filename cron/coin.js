8bbeed3d4f578a27abd882468443545d8b843f41
Akshay <akshaycm@hotmail.com>
2019-05-25 22:55:06 +0300

    remove burnt supply from moneysupply

--- cron/coin.js
+++ cron/coin.js
@@ -1,4 +1,3 @@
-
 require('babel-polyfill');
 const config = require('../config');
 const { exit, rpc } = require('../lib/cron');
@@ -13,59 +12,59 @@ const Coin = require('../model/coin');
  * like price coinmarketcap.com data.
  */
 async function syncCoin() {
-  const date = moment().utc().startOf('minute').toDate();
-  // Setup the coinmarketcap.com api url.
-  const url = `${ config.coinMarketCap.api }${ config.coinMarketCap.ticker }`;
+    const date = moment().utc().startOf('minute').toDate();
+    // Setup the coinmarketcap.com api url.
+    const url = `${ config.coinMarketCap.api }${ config.coinMarketCap.ticker }`;
 
-  const info = await rpc.call('getinfo');
-  const masternodes = await rpc.call('getmasternodecount');
-  const nethashps = await rpc.call('getnetworkhashps');
+    const info = await rpc.call('getinfo');
+    const masternodes = await rpc.call('getmasternodecount');
+    const nethashps = await rpc.call('getnetworkhashps');
 
-  let market = await fetch(url);
-  if (Array.isArray(market)) {
-    market = market.length ? market[0] : {};
-  }
+    let market = await fetch(url);
+    if (Array.isArray(market)) {
+        market = market.length ? market[0] : {};
+    }
 
-  const coin = new Coin({
-    cap: market.market_cap_usd,
-    createdAt: date,
-    blocks: info.blocks,
-    btc: market.price_btc,
-    diff: info.difficulty,
-    mnsOff: masternodes.total - masternodes.stable,
-    mnsOn: masternodes.stable,
-    netHash: nethashps,
-    peers: info.connections,
-    status: 'Online',
-    supply: info.moneysupply, // TODO: change to actual count from db.
-    usd: market.price_usd
-  });
+    const coin = new Coin({
+        cap: market.market_cap_usd,
+        createdAt: date,
+        blocks: info.blocks,
+        btc: market.price_btc,
+        diff: info.difficulty,
+        mnsOff: masternodes.total - masternodes.stable,
+        mnsOn: masternodes.stable,
+        netHash: nethashps,
+        peers: info.connections,
+        status: 'Online',
+        supply: info.moneysupply - 1217064.07, // TODO: change to actual count from db.
+        usd: market.price_usd
+    });
 
-  await coin.save();
+    await coin.save();
 }
 
 /**
  * Handle locking.
  */
 async function update() {
-  const type = 'coin';
-  let code = 0;
+    const type = 'coin';
+    let code = 0;
 
-  try {
-    locker.lock(type);
-    await syncCoin();
-  } catch(err) {
-    console.log(err);
-    code = 1;
-  } finally {
     try {
-      locker.unlock(type);
-    } catch(err) {
-      console.log(err);
-      code = 1;
+        locker.lock(type);
+        await syncCoin();
+    } catch (err) {
+        console.log(err);
+        code = 1;
+    } finally {
+        try {
+            locker.unlock(type);
+        } catch (err) {
+            console.log(err);
+            code = 1;
+        }
+        exit(code);
     }
-    exit(code);
-  }
 }
 
 update();
