3e1fe72f3183419bac17d3e7fb90d0eeccff765e
Akshay <akshaycm@hotmail.com>
2019-05-31 20:23:17 +0300

    [Icons] Update icons

--- client/component/Menu/menuData.js
+++ client/component/Menu/menuData.js
@@ -1,14 +1,14 @@
 const MenuData = [
-  {label: 'Overview', icon: '/img/home.svg', href: '/'},
-  {label: 'Movement', icon: '/img/movement.svg', href: '/movement'},
-  {label: 'Market', icon: '/img/chart.svg', href: '/market'},
-  {label: 'Top 100', icon: '/img/top100.svg', href: '/top'},
-  {label: 'Masternode', icon: '/img/masternodes.svg', href: '/masternode'},
-  {label: 'Governance', icon: '/img/masternodes.svg', href: '/governance'},
-  {label: 'Connections', icon: '/img/connections.svg', href: '/peer'},
-  {label: 'Statistics', icon: '/img/statistics.svg', href: '/statistics'},
-  {label: 'Coin Info', icon: '/img/coininfo.svg', href: '/coin'},
-  {label: 'API', icon: '/img/api.svg', href: '/api'}
+  { label: 'Overview', icon: '/img/home.svg', href: '/' },
+  { label: 'Movement', icon: '/img/movement.svg', href: '/movement' },
+  { label: 'Market', icon: '/img/market.svg', href: '/market' },
+  { label: 'Top 100', icon: '/img/top100.svg', href: '/top' },
+  { label: 'Masternode', icon: '/img/masternodes.svg', href: '/masternode' },
+  { label: 'Governance', icon: '/img/goverance.svg', href: '/governance' },
+  { label: 'Connections', icon: '/img/connections.svg', href: '/peer' },
+  { label: 'Statistics', icon: '/img/statistics.svg', href: '/statistics' },
+  { label: 'Coin Info', icon: '/img/coininfo.svg', href: '/coin' },
+  { label: 'API', icon: '/img/api.svg', href: '/api' }
 ]
 
 export default MenuData;
