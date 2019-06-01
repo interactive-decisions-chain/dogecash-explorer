16890469ba2ae713ee4bd6a4e7110284e7032200
Akshay <akshaycm@hotmail.com>
2019-06-01 22:57:59 +0300

    dont show new cards for rewards if its undefined

--- client/container/TX.jsx
+++ client/container/TX.jsx
@@ -55,7 +55,7 @@ class TX extends Component {
   }
 
   getBlockRewardDetailsMasternode() {
-    if (!this.props.tx.isReward) {
+    if (!this.props.tx.isReward || this.props.state.blockRewardDetails === undefined) {
       return null;
     }
     return (
@@ -67,7 +67,7 @@ class TX extends Component {
   }
 
   getBlockRewardDetailsStaking() {
-    if (!this.props.tx.isReward) {
+    if (!this.props.tx.isReward || this.props.state.blockRewardDetails === undefined) {
       return null;
     }
     return (
