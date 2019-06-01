8b510eb11646fe28f2a8fc5d576d1946352103c1
Akshay <akshaycm@hotmail.com>
2019-06-01 18:40:23 +0300

    revert method

--- server/handler/blockex.js
+++ server/handler/blockex.js
@@ -465,7 +465,7 @@ const getAddnodes = async(req, res) => {
         .limit(req.query.limit ? parseInt(req.query.limit, 10) : 500)
         .sort({ ip: 1 })
         .then((docs) => {
-            var returndata = JSON.parse(JSON.stringify(docs));
+            var returndata = docs;
             for (var i = 0; i < returndata.length; i++) {
                 returnaddnodes = returnaddnodes + "addnode=" + returndata.ip[i] + "\n"
             }
