2e082d727649ac7cea3e37e3ef1b871d1eaa84e9
Akshay <akshaycm@hotmail.com>
2019-06-03 13:41:52 +0300

    Merge pull request #106 from hodlforjesus/fb-detailed-rewards
    Improved cron block locking

--- cron/block.js
+++ cron/block.js
@@ -46,6 +46,7 @@ async function syncBlocks(start, stop, clean = false) {
 
         await block.save();
 
+    // Notice how we're ensuring to only use a single rpc call with forEachSeries()
     await forEachSeries(block.txs, async (txhash) => {
       const rpctx = await util.getTX(txhash, true);
 
@@ -150,19 +151,25 @@ async function update() {
             dbHeight = 1;
         }
 
-        await syncBlocks(dbHeight, rpcHeight, clean);
-    } catch (err) {
-        console.log(err);
-        code = 1;
-    } finally {
-        try {
-            locker.unlock(type);
-        } catch (err) {
-            console.log(err);
-            code = 1;
-        }
-        exit(code);
+    // If nothing to do then exit.
+    if (dbHeight >= rpcHeight) {
+      locker.unlock(type); // Be sure to properly unlock cron
+      return;
     }
+    // If starting from genesis skip.
+    else if (dbHeight === 0) {
+      dbHeight = 1;
+    }
+
+    await syncBlocks(dbHeight, rpcHeight, clean);
+
+    locker.unlock(type); // It is important that we keep proper lock during syncing otherwise there will be blockchain data corruption and we can't be sure of integrity
+  } catch (err) {
+    console.log(err);
+    code = 1;
+  } finally {
+    exit(code);
+  }
 }
 
 update();
