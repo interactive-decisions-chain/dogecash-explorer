e4f1bd55d3a2c8b477c3c589e785ec99b2067edb
Akshay <akshaycm@hotmail.com>
2019-06-28 19:19:29 +0300

    fixes for model

--- model/tx.js
+++ model/tx.js
@@ -1,122 +1,69 @@
-
 const mongoose = require('mongoose');
 
 /**
  * When a vin is spent, we'll have extra data of what was spent
  */
 const RelatedVout = new mongoose.Schema({
-  address: { index: false, required: true, type: String },
-  addressBalance: { index: true, required: true, type: Number }, // Store address balance before the transaction input was spent
-
-  value: { required: true, type: Number },
-  confirmations: { required: true, type: Number },
-  date: { index: true, required: true, type: Date },
-  age: { index: true, required: true, type: Number },  
+    address: { index: false, required: true, type: String },
+    value: { required: true, type: Number },
+    confirmations: { required: true, type: Number },
+    date: { index: true, required: true, type: Date },
+    age: { index: true, required: true, type: Number },
 }, { _id: false, versionKey: false });
 
 /**
  * Sometimes we can store the asm instruction of the script sig (ex: to identify ZEROCOIN transactions)
  */
 const ScriptSig = new mongoose.Schema({
-  asm: { type: String },
+    asm: { type: String },
 }, { _id: false, versionKey: false });
 
 /**
  * The inputs for a tx.
  */
 const TXIn = new mongoose.Schema({
-  coinbase: { type: String },
-  //sequence: { type: Number },
-  txId: { type: String },
-  vout: { type: Number },
-  relatedVout: { required: false, type: RelatedVout },
-  scriptSig: { required: false, type: ScriptSig }
+    coinbase: { type: String },
+    //sequence: { type: Number },
+    txId: { type: String },
+    vout: { type: Number },
+    relatedVout: { required: false, type: RelatedVout },
+    scriptSig: { required: false, type: ScriptSig }
 }, { _id: false, versionKey: false });
 
 /**
  * The outputs for a tx.
  */
 const TXOut = new mongoose.Schema({
-  address: { index: true, required: true, type: String },
-  n: { required: true, type: Number },
-  value: { required: true, type: Number }
+    address: { index: true, required: true, type: String },
+    n: { required: true, type: Number },
+    value: { required: true, type: Number }
 }, { _id: false, versionKey: false });
-
-/**
- * Structure for detailed breakdown of the staking reward
- */
-const BlockRewardDetailsStakeInput = new mongoose.Schema({
-  txId: { index: false, required: true, type: String },
-  amount: { index: false, required: true, type: Number },
-  confirmations: { index: false, required: true, type: Number },
-  date: { index: false, required: true, type: Date },
-  age: { index: false, required: true, type: Number },
-});
-/**
- * Structure for detailed breakdown of the staking input
- */
-const BlockRewardDetailsStakeReward = new mongoose.Schema({
-  amount: { index: false, required: true, type: Number },
-  address: { index: false, required: true, type: String },
-});
-
-/**
- * Structure for detailed breakdown of the stake
- */
-const BlockRewardDetailsStake = new mongoose.Schema({
-  address: { index: false, required: true, type: String },
-  input: { index: false, required: true, type: BlockRewardDetailsStakeInput },
-  reward: { index: false, required: true, type: BlockRewardDetailsStakeReward },
-});
-
-/**
- * Structure for detailed breakdown of the masternode reward
- */
-const BlockRewardDetailsMasternodeReward = new mongoose.Schema({
-  amount: { index: false, required: true, type: Number },
-  address: { index: false, required: true, type: String },
-});
-/**
- * Structure for detailed breakdown of the stake
- */
-const BlockRewardDetailsMasternode = new mongoose.Schema({
-  reward: { index: false, required: true, type: BlockRewardDetailsMasternodeReward },
-});
-
-/**
- * Structure for detailed breakdown of the reward
- */
-const BlockRewardDetails = new mongoose.Schema({
-  stake: { index: false, required: true, type: BlockRewardDetailsStake },
-  masternode: { index: false, required: true, type: BlockRewardDetailsMasternode },
-});
-
 
 /**
  * Setup the schema for transactions.
  */
 const txSchema = new mongoose.Schema({
-  __v: { select: false, type: Number },
-  _id: mongoose.Schema.Types.ObjectId,
-  //blockHash: { required: true, type: String },
-  blockHeight: { index: true, required: true, type: Number },
-  createdAt: { index: true, required: true, type: Date },
-  txId: { index: true, required: true, type: String },
-  version: { required: true, type: Number },
-  vin: { required: true, type: [TXIn] },
-  vout: { required: true, type: [TXOut] },
-  isReward: { required: false, type: Boolean },
-  blockRewardDetails: { type: mongoose.Schema.Types.ObjectId, ref: 'BlockRewardDetails' },
-  involvedAddresses: { required: true, type: [String], index: true }
+    __v: { select: false, type: Number },
+    _id: mongoose.Schema.Types.ObjectId,
+    //blockHash: { required: true, type: String },
+    blockHeight: { index: true, required: true, type: Number },
+    createdAt: { index: true, required: true, type: Date },
+    txId: { index: true, required: true, type: String },
+    version: { required: true, type: Number },
+    vin: { required: true, type: [TXIn] },
+    vout: { required: true, type: [TXOut] },
+    isReward: { required: false, type: Boolean },
+    blockRewardDetails: { type: mongoose.Schema.Types.ObjectId, ref: 'BlockRewardDetails' },
+    involvedAddresses: { required: true, type: [String], index: true }
 }, { versionKey: false });
 
 /**
  * Helper method to return vout value for tx.
  */
 txSchema.virtual('value')
-  .get(() => {
-    return this.vout.reduce((acc, vo) => acc + vo.value, 0.0);
-  });
+    .get(() => {
+        return this.vout.reduce((acc, vo) => acc + vo.value, 0.0);
+    });
 
 /**
  * The transaction object.  Very basic as
